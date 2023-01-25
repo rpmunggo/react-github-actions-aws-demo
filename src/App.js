7d71eb16e91f9f74b0bc54274661de95d6d17ae1
Rogelio Pascual III <rogelio@prosperna.com>
2023-01-25 14:06:09 +0800

    demo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Please work! Please please please. Tulog na ko please.
+          Hello blog, welcome to my guys. uWu
         </p>
         <a
           className="App-link"
